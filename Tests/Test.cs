2972d9c8bf3b05b67c0b39ab8de7d8f8e50d9f25
Alexander <sania.sviridenko@gmail.com>
2018-05-26 20:44:40 +0300

    final config ci

---
+++ Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using GameStore.Controllers;
+using System.Web.Mvc;
+using Xunit;
+
+namespace Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.NotNull(result);
+        }
+    }
+}

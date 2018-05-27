6a8a3e4da9c8ea6359a309af897089f5e0ff8954
Alexander <sania.sviridenko@gmail.com>
2018-05-27 23:13:09 +0300

    add domain project

---
+++ StoreDomain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace StoreDomain.Concrete
+{
+    class EFDbContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<ForeignKeyIndexConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+        }
+    }
+}
